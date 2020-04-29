5acec9b8048412eab573f9b7032b1d5421c0680b
unknown <tsai>
2020-04-29 10:49:12 +0800

    update

---
+++ app.py
@@ -0,0 +1,157 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, render_template, request, redirect, flash
+from flask import url_for
+from flask import Flask
+
+app = Flask(__name__)
+
+
+POSTGRES = {
+    'user': 'test',
+    'pw': 'test',
+    'db': 'Student',
+    'host': 'localhost',
+    'port': '5432',
+}
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+db = SQLAlchemy(app)
+
+Grade = db.Table('grade' ,db.metadata ,autoload = True ,autoload_with = db.engine)
+Course = db.Table('course' ,db.metadata ,autoload = True ,autoload_with = db.engine)
+tudent = db.Table('student' ,db.metadata ,autoload = True ,autoload_with = db.engine)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/delete')
+def delete():
+    return render_template('delete.html')
+
+@app.route('/modify')
+def modify():
+    return render_template('modify.html')
+
+@app.route('/insert')
+def insert():
+    return render_template('add.html')
+
+@app.route('/search_table',methods=['POST'])
+def search_table():
+    if request.method == 'POST':
+        table_name = request.form.get('table_name')
+        # temp = db.Table(table_name ,db.metadata ,autoload = True ,autoload_with = db.engine)
+        # results = db.session.query(temp).all()
+        results = db.session.execute("select * from %s" %(table_name))
+        title = db.session.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='%s'"%(table_name))
+        return render_template('search.html', output_data = results.fetchall() ,title = title)
+
+@app.route('/insert_get_data',methods=['POST'])
+def insert_get_data():
+    if request.method == 'POST':
+        table_name = request.form.get('table_name')
+        results = db.session.execute("select * from %s" %(table_name))
+        title = db.session.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='%s'"%(table_name))
+        return render_template('add.html', output_data = results.fetchall() ,title = title ,table_name = table_name)
+
+@app.route('/delete_get_data',methods=['POST'])
+def delete_get_data():
+    if request.method == 'POST':
+        table_name = request.form.get('table_name')
+        results = db.session.execute("select * from %s" %(table_name))
+        title = db.session.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='%s'"%(table_name))
+        return render_template('delete.html', output_data = results.fetchall() ,title = title ,table_name = table_name)
+
+@app.route('/Modify_get_data',methods=['POST'])
+def Modify_get_data():
+    if request.method == 'POST':
+        table_name = request.form.get('table_name')
+        results = db.session.execute("select * from %s" %(table_name))
+        title = db.session.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='%s'"%(table_name))
+        return render_template('modify.html', output_data = results.fetchall() ,title = title ,table_name = table_name)
+
+
+@app.route('/insert_data',methods=['POST'])
+def insert_data():
+    if request.method == 'POST':
+        table_name = request.form.get('table_name')
+        if table_name == 'grade':
+            student_id	= request.form.get('student_id')
+            course_id = request.form.get('course_id')
+            score = request.form.get('score')
+            sql = "INSERT INTO %s (student_id, course_id, score) VALUES (%s,%s,%s);"%(table_name ,student_id ,course_id ,score)
+        elif table_name == 'course':
+            course_id	= request.form.get('course_id')
+            course_name = request.form.get('course_name')
+            credit = request.form.get('credit')
+            sql = "INSERT INTO %s (course_id, course_name, credit) VALUES (%s,%s,%s);"%(table_name ,course_id ,course_name ,credit)
+        elif table_name == 'student':
+            student_id	= request.form.get('student_id')
+            student_name = request.form.get('student_name')
+            gender = request.form.get('gender')
+            birthday = request.form.get('birthday')
+            fruit = request.form.get('fruit')
+            sql = "INSERT INTO %s (student_name, gender, birthday,fruit) VALUES (%s,%s,%s,%s,%s);"%(table_name ,student_id ,student_name ,gender,birthday,fruit)
+        db.session.execute(sql)
+        db.session.commit()
+        results = db.session.execute("select * from %s" %(table_name))
+        title = db.session.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='%s'"%(table_name))
+        
+        return render_template('search.html', output_data = results.fetchall() ,title = title)
+
+@app.route('/delete_data',methods=['POST'])
+def delete_data():
+    if request.method == 'POST':
+        table_name = request.form.get('table_name')
+        if table_name == 'grade':
+            student_id	= request.form.get('student_id')
+            course_id = request.form.get('course_id')
+            sql = "DELETE FROM %s WHERE student_id ='%s' and course_id = '%s'" %(table_name ,student_id ,course_id)
+        elif table_name == 'course':
+            course_id	= request.form.get('course_id')
+            sql = "DELETE FROM %s WHERE course_id='%s'" %(table_name ,course_id)
+        elif table_name == 'student':
+            student_id	= request.form.get('student_id')
+            sql = "DELETE FROM %s WHERE student_id='%s'" %(table_name ,student_id)
+        db.session.execute(sql)
+        db.session.commit()
+        results = db.session.execute("select * from %s" %(table_name))
+        title = db.session.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='%s'"%(table_name))
+        return render_template('search.html', output_data = results.fetchall() ,title = title)
+
+@app.route('/modify_data',methods=['POST'])
+def modify_data():
+    if request.method == 'POST':
+        table_name = request.form.get('table_name')
+        if table_name == 'grade':
+            student_id	= request.form.get('student_id')
+            course_id = request.form.get('course_id')
+            score = request.form.get('score')
+            sql = "UPDATE %s SET score = %s WHERE student_id = '%s' and course_id ='%s'"%(table_name ,score ,student_id ,course_id)
+        elif table_name == 'course':
+            course_id	= request.form.get('course_id')
+            course_name = request.form.get('course_name')
+            credit = request.form.get('credit')
+            sql = "UPDATE %s SET course_name ='%s' ,credit =%s WHERE course_id ='%s'"%(table_name ,course_name,credit ,course_id)
+        elif table_name == 'student':
+            student_id	= request.form.get('student_id')
+            student_name = request.form.get('student_name')
+            gender = request.form.get('gender')
+            birthday = request.form.get('birthday')
+            fruit = request.form.get('fruit')
+            sql = "UPDATE %s SET student_name ='%s',gender='%s',birthday='%s',fruit='%s' WHERE student_id ='%s'"%(table_name ,student_name,gender ,birthday,fruit,student_id) 
+        db.session.execute(sql)
+        db.session.commit()
+        results = db.session.execute("select * from %s" %(table_name))
+        title = db.session.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='%s'"%(table_name))
+        return render_template('search.html', output_data = results.fetchall() ,title = title)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
